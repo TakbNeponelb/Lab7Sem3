401683ea82f4669270861df499b9badd0d87e855
Max Tatarnikov <tatmaks0@gmail.com>
2023-11-13 11:49:30 +0300

    Пробую разобраться с git

---
+++ Lab7Sem3/Lab7Sem3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7Sem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sosi");
+            _ = Console.ReadLine();
+        }
+    }
+}
